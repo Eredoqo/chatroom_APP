3343eb5e14770d03a21ae8f1c130c6e4809c3e74
Eredoqo <eredoqorri96@gmail.com>
2022-06-08 10:48:16 +0000

    git commit

---
+++ hasura-ecomm/migrations/default/1654685155708_users/up.sql
@@ -0,0 +1,9 @@
+        CREATE TABLE users(
+
+                      user_id INT,
+
+                      username VARCHAR(255) NOT NULL,
+
+                      PRIMARY KEY(user_id)
+
+            );
