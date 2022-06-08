440689bcd78b56c9381613629723ccbf0fa98d3f
Eredoqo <eredoqorri96@gmail.com>
2022-06-08 11:31:11 +0000

    git commit

--- hasura-ecomm/migrations/default/1654685790153_users/up.sql
+++ hasura-ecomm/migrations/default/1654685790153_users/up.sql
@@ -1,4 +1,3 @@
-   CREATE TABLE IF NOT EXIST "public"."users"( 
-       "user_id" INT GENERATED ALWAYS AS IDENTITY,
-        "username" VARCHAR(255) NOT NULL, 
-        PRIMARY KEY("user_id") );
+   CREATE TABLE IF NOT EXIST "users"  (
+       "id"SERIAL PRIMARY KEY
+   );
